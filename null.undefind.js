2b38360ce66dc12213b4cd26348bfe29a1284fb5
ashraful132 <ashrafful1600@gmail.com>
2024-09-23 20:25:12 +0600

    duble

---
+++ null.undefind.js
@@ -0,0 +1,7 @@
+function add(num1,num2){
+    console.log(num1 + num2);
+    return false
+}
+
+const result = add (13,82);
+console.log(result)
---
+++ true-false.js
@@ -0,0 +1,15 @@
+// falsy:
+// false
+// 0
+// ""
+// udenfined
+// null
+// NaN
+// truthy
+let name = {};
+if(nam){
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
